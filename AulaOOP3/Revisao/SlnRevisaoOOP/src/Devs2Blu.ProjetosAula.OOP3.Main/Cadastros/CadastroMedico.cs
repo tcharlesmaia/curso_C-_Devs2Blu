0cc32835e40b5c1bf8b28f16d292a12870fa0ede
Tcharles da Maia <tcharlesmaia@yahoo.com.br>
2022-09-28 18:36:42 -0300

    polimorfismo

---
+++ AulaOOP3/Revisao/SlnRevisaoOOP/src/Devs2Blu.ProjetosAula.OOP3.Main/Cadastros/CadastroMedico.cs
@@ -0,0 +1,61 @@
+﻿using Devs2Blu.ProjetosAula.OOP3.Main.Interfaces;
+using Devs2Blu.ProjetosAula.OOP3.Main.Utils.Enums;
+using Devs2Blu.ProjetosAula.OOP3.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs2Blu.ProjetosAula.OOP3.Main.Cadastros
+{
+    public class CadastroMedico : IMenuCadastro
+    {
+        public Int32 MenuCadastro()
+        {
+            Int32 opcao;
+
+            Console.Clear();
+            Console.WriteLine("----- Cadastro de Medicos -----");
+            Console.WriteLine("----- 1- Lista de Medicos -----");
+            Console.WriteLine("----- 2- Cadastro de Medicos -----");
+            Console.WriteLine("----- 3- Alterar Medicos -----");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("----- 0- Sair -----");
+            Int32.TryParse(Console.ReadLine(), out opcao);
+            return opcao;
+
+        }
+
+        public void Listar()
+        {
+            Console.Clear();
+
+            foreach (var medico in Program.Mock.ListaMedicos)
+            {
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine($"Medico: {medico.CodigoMedico}");
+                Console.WriteLine($"Nome: {medico.Nome}");
+                Console.WriteLine($"Especialidade: {medico.Especialidade}");
+                Console.WriteLine($"CRM: {medico.CRM}");
+                Console.WriteLine("-----------------------------------------\n");
+            }
+            Console.ReadLine();
+        }
+
+        public void Cadastrar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Alterar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- AulaOOP3/Revisao/SlnRevisaoOOP/src/Devs2Blu.ProjetosAula.OOP3.Main/Cadastros/CadastroPaciente.cs
+++ AulaOOP3/Revisao/SlnRevisaoOOP/src/Devs2Blu.ProjetosAula.OOP3.Main/Cadastros/CadastroPaciente.cs
@@ -3,46 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Devs2Blu.ProjetosAula.OOP3.Main.Interfaces;
 using Devs2Blu.ProjetosAula.OOP3.Main.Utils;
 using Devs2Blu.ProjetosAula.OOP3.Main.Utils.Enums;
 using Devs2Blu.ProjetosAula.OOP3.Models.Model;
 
 namespace Devs2Blu.ProjetosAula.OOP3.Main.Cadastros
 {
-    public class CadastroPaciente
+    public class CadastroPaciente : IMenuCadastro
     {
         public CadastroPaciente()
         {
 
         }
-
-        public void MenuCadastro()
-        {
-            Int32 opcao;
-
-            do
-            {
-                Console.WriteLine("----- Cadastro de Pacientes -----");
-                Console.WriteLine("----- 1- Lista de Pacientes -----");
-                Console.WriteLine("----- 2- Cadastro de Pacientes -----");
-                Console.WriteLine("----- 3- Alterar Pacientes -----");
-                Console.WriteLine("---------------------");
-                Console.WriteLine("----- 0- Sair -----");
-                Int32.TryParse(Console.ReadLine(), out opcao);
-
-                switch (opcao)
-                {
-                    case (int)MenuEnums.LISTAR:
-                        ListarPacientes();
-                        break;
-                    default:
-                        break;
-                }
-
-            } while (!opcao.Equals((int)MenuEnums.SAIR));
-        }
-
-        public void ListarPacientes()
+        private void ListarPacientes()
         {
             Console.Clear();
 
@@ -55,21 +29,63 @@ namespace Devs2Blu.ProjetosAula.OOP3.Main.Cadastros
                 Console.WriteLine($"Convenio: {paciente.Convenio}");
                 Console.WriteLine("-----------------------------------------\n");
             }
+            Console.ReadLine();
         }
 
-        public void CadastrarPaciente()
+        private void CadastrarPaciente(Paciente novoPaciente)
+        {
+            Program.Mock.ListaPacientes.Add(novoPaciente);
+        }
+
+        private void AlterarPaciente(Paciente paciente)
         {
 
         }
 
-        public void AlterarPaciente()
+        private void ExcluirPaciente(Paciente paciente)
         {
 
         }
 
-        public void ExcluirPaciente()
-        {
 
+        #region FACADE
+        public Int32 MenuCadastro()
+        {
+            Int32 opcao;
+            Console.Clear();
+            Console.WriteLine("----- Cadastro de Pacientes -----");
+            Console.WriteLine("----- 1- Lista de Pacientes -----");
+            Console.WriteLine("----- 2- Cadastro de Pacientes -----");
+            Console.WriteLine("----- 3- Alterar Pacientes -----");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("----- 0- Sair -----");
+            Int32.TryParse(Console.ReadLine(), out opcao);
+            return opcao;
         }
+
+        public void Listar()
+        {
+            ListarPacientes();
+        }
+
+        public void Cadastrar()
+        {
+            Paciente paciente = new Paciente();
+            CadastrarPaciente(paciente);
+        }
+
+        public void Alterar()
+        {
+            Paciente paciente = new Paciente();
+            AlterarPaciente(paciente);
+        }
+
+        public void Excluir()
+        {
+            Paciente paciente = new Paciente();
+            ExcluirPaciente(paciente);
+        }
+
+        #endregion
     }
 }
