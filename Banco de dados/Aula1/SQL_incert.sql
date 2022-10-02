0a317127c8236f2ae0bc37a8c41090f3fbaa6d37
Tcharles da Maia <tcharlesmaia@yahoo.com.br>
2022-10-02 16:35:47 -0300

    Atualização atividade avaliativa

---
+++ Banco de dados/Aula1/SQL_incert.sql
@@ -0,0 +1,5 @@
+insert into usuario (id_pessoa, loging, senha, status)
+values (1, 'Tcharles', '1234', 'A'), (2, 'Bernardo', '123', 'B'), (3, 'Cassio', '123', 'C'),
+		(4, 'Duda', '321', 'D'), (5, 'EDu', '456', 'A');
+
+        
